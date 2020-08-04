5197c2eed018cd8150036a5ec72cbb4d66182ae9
HeathBanner <heathbanner@outlook.com>
2020-08-04 13:54:42 -0400

    Fixed shallow copy problem with object manipulation in the handle functions

--- personal-test/src/pages/Test/services/index.js
+++ personal-test/src/pages/Test/services/index.js
@@ -1,38 +1,59 @@
-export function handleMultiple(guess, answer, state) {
+export function handleMultiple(guess, state, questionData) {
+    const answer = questionData[state.question - 1].answer;
+    const isFinished = state.question >= 10;
+    let reviewData = [];
+
     if (guess !== answer) {
-        Object.assign({}, state, {
-            question: state.question++,
-            incorrect: state.incorrect++,
-            checked: []
-        });
+        state.review.push(state.question - 1);
+        state.incorrect++;
+        state.checked = [];
     } else {
-        Object.assign({}, state, {
-            question: state.question++,
-            correct: state.correct++,
-            checked: []
-        });
+        state.correct++;
+        state.checked = [];
     }
+    if (!isFinished) state.question++;
+    if (state.review.length > 0 && isFinished) {
+        reviewData = processReview(state, questionData);
+        state.reviewData = reviewData;
+        state.finished = true;
+    }
+
     return state;
 };
 
-export function handleCheckbox(answer, state) {
+export function handleCheckbox(state, questionData) {
+    const answer = questionData[state.question - 1].answer;
+    const isFinished = state.question >= 10;
+    let reviewData = [];
     let score = 0;
+
     state.checked.forEach((option) => {
         if (answer.indexOf(option) >= 0) score++;
         else score--;
     });
+
     if (score < answer.length) {
-        Object.assign({}, state, {
-            question: state.question++,
-            incorrect: state.incorrect++,
-            checked: []
-        });
+        state.review.push(parseInt(state.question - 1));
+        state.incorrect++
+        state.checked = [];
     } else {
-        Object.assign({}, state, {
-            question: state.question++,
-            correct: state.correct++,
-            checked: []
-        });
+        state.correct++;
+        state.checked = [];
     }
+    if (state.review.length > 0 && isFinished) {
+        reviewData = processReview(state, questionData);
+        state.reviewData = reviewData;
+        state.finished = true;
+    }
+    if (!isFinished) state.question++;
+
     return state;
 };
+
+function processReview(state, questionData) {
+    const reviewData = state.review.map((question) => {
+        return questionData[question];
+    });
+
+    return reviewData;
+}
