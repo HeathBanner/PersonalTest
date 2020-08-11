5fb450b88fd6b5d6bd4a1860caf6179ea502f602
HeathBanner <heathbanner@outlook.com>
2020-08-11 10:32:09 -0400

    Changed from .js to .tsx

---
+++ personal-test/src/components/Test/GameOver.tsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+export default class GameOver extends PureComponent {
+    render() {
+        return (
+            <>
+                <h1>Finished!</h1>            
+            </>
+        );
+    };
+};
